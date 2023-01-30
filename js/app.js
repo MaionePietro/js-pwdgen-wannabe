841700f50d304bf7fc9cbddf9aae00822c6d8a26
MaionePietro <pietromaionespam@gmail.com>
2023-01-30 13:59:38 +0100

    FEAT console.lod (nome/cognome/colore/numero)

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
+alert('Benvenuto in password genetator');
+let nome, cognome, colore, numero;
+
 // 1 Chiedi all’utente il suo nome,
 
-
-
-
+nome = prompt('inserisci il tuo nome')
+console.log(nome);
 
 // 2 poi chiedi il suo cognome,
 
-
-
-
+cognome = prompt('inserisci il tuo cognome')
+console.log(cognome);
 
 // 3 poi chiedi il suo colore preferito
 
-
-
-
+colore = prompt('inserisci il tuo colore preferito')
+console.log(colore);
 
 // 4 e un numero
 
-
-
-
+numero = prompt('inserisci un numero')
+console.log(numero);
 
 // 5 Infine scrivi sulla pagina il risultato della concatenazione di: <nome><cognome><colorePreferito><numero></numero>
