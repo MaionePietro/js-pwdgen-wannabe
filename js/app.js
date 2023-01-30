875170f59f5c83dea5015542ee97ea97c924c483
MaionePietro <pietromaionespam@gmail.com>
2023-01-30 13:45:54 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// 1 Chiedi all’utente il suo nome,
+
+
+
+
+
+// 2 poi chiedi il suo cognome,
+
+
+
+
+
+// 3 poi chiedi il suo colore preferito
+
+
+
+
+
+// 4 e un numero
+
+
+
+
+
+// 5 Infine scrivi sulla pagina il risultato della concatenazione di: <nome><cognome><colorePreferito><numero></numero>
